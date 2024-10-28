6ed6da0e30bf4afd725dd9935a383237e097d5f0
Jijo <jijo.eramplackal@gmail.com>
2024-10-28 16:24:38 +0530

    c2

--- Todo/settings.py
+++ Todo/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,6 +91,7 @@ DATABASES = {
     }
 }
 
+DATABASES['default']=dj_database_url.config(conn_max_age=600,ssl_required=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
